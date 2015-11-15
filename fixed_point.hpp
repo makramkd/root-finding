b80d35d9eee2cbaae3461d100bb80af32bf31b19
Makram Kamaleddine <makram.kd@gmail.com>
2015-11-14 20:59:54 -0500

    Adding the old code from main into the fixed_point.hpp file.

--- fixed_point.hpp
+++ fixed_point.hpp
@@ -95,6 +95,37 @@ namespace fp {
         file << "END" << std::endl;
     }
 
+    void test()
+    {
+        const auto g1 = [](double x) -> double {
+            return (x * x + 2) / 3;
+        };
+
+        // will result in NaNs because the value under the square root
+        // will become negative at some point.
+        const auto g2 = [](double x) -> double {
+            return std::sqrt(3 * x - 2);
+        };
+
+        const auto g3 = [](double x) -> double {
+            return 3 - (2 / x);
+        };
+
+        const auto g4 = [](double x) -> double {
+            return (x * x  - 2) / (2 * x - 3);
+        };
+
+        std::vector<std::function<double(double)>> vec {g1, g2, g3, g4};
+        std::vector<std::string> filenames {"g1.txt", "g2.txt", "g3.txt", "g4.txt"};
+        std::vector<std::string> funcnames {"g1", "g2", "g3", "g4"};
+        const double x0 = 0;
+        const double abstol = 5e-7;
+
+        for (auto i = 0; i < vec.size(); ++i) {
+            fp::test_fixed_point(vec[i], x0, abstol, funcnames[i], filenames[i]);
+        }
+    }
+
     template<typename Func, typename Float>
     std::tuple<std::vector<Float>, int, std::vector<Float>> newton_method(const Func& f, Float x0, Float abstol)
     {
